56abd04533be3a5b03345e02eb93828809f63781
Jay Patel <85619425+dev2529@users.noreply.github.com>
2024-03-23 21:58:34 -0400

    second commit

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name='mcq-generator',version='0.0.1',author='Jay Patel',author_email='jayppatel3627@gmail.com',packages=find_packages(),install_requires=[install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"])
